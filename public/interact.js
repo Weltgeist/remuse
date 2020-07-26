c9ef8639df9012240c98fdf179f5c2ecf8333b79
Weltgeist <narcisse118@gmail.com>
2020-07-25 23:57:54 -0400

    loading keys of skill header to expends

--- public/interact.js
+++ public/interact.js
@@ -1,1 +1,7 @@
 //Mouse and event interactions
+
+const key = ['control-system', 'web-dev', 'emb-dev', 'mechanical-eng'];
+const skillsToOpen = [];
+keys.forEach(function(key){
+  skillsToOpen.push(document.getElementById(key));
+})
