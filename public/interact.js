e31d36eab2155b2e3f8ef748b5e31ffde07919f0
Weltgeist <narcisse118@gmail.com>
2020-07-27 23:53:11 -0400

    added more content to DOM event functions

--- public/interact.js
+++ public/interact.js
@@ -1,19 +1,27 @@
 //Mouse and event interactions
 
-const key = ['control-system', 'web-dev', 'emb-dev', 'mechanical-eng'];
+const keyOpen = ['control-system-open', 'web-dev-open', 'emb-dev-open', 'mechanical-eng-open'];
+const keyClose = ['control-system-close', 'web-dev-close', 'emb-dev-close', 'mechanical-eng-close'];
+
 const skillsToOpen = [];
-keys.forEach(function(key){
+keyOpen.forEach(function(key){
   skillsToOpen.push(document.getElementById(key));
 })
 
-const skillsTreeOpen = (event) => {
+const skillsToClose = [];
+keyClose.forEach(function(key){
+  skillsToClose.push(document.getElementById(key));
+})
 
+const skillsTreeOpen = (event) => {
+  event.target.style.display = inline-block;
 }
 
 const skillsTreeClose = (event) => {
-  
+  event.target.style.display = none;
 }
 
 const skillTreeManager = (skillTree) => {
   skillTree.addEventListener('click',skillsTreeOpen);
+  skillTree.addEventListener('click',skillsTreeClose);
 }
