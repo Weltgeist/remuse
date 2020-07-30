25ea99d26bc3744d225fce8877135c35efecaac2
Weltgeist <narcisse118@gmail.com>
2020-07-29 23:40:05 -0400

    added content to job desc

--- public/main.js
+++ public/main.js
@@ -7,7 +7,7 @@ const context = {
       image: "https://media-exp1.licdn.com/dms/image/C4E0BAQGBZhibjnUZuQ/company-logo_200_200/0?e=2159024400&v=beta&t=Hun9oGD0r1_Hp2OMEwZvOf0jacz_rEHXNSm1e9EQ-jg",
       jobTitle: "Eng. Consultant - Part time/Online",
       workplace: "Upwork & Utest",
-      jobDescription: [ "Offer engineering services in projects such as: Robotic Control System Design & Software development (C/C++/Matlab); Mechanical design of movie props (Solidworks), Software Translation (C++/Python/Matlab)", "Offer consulting expertise in Multiple S/W & H/W functional testing projects for products, applications & websites from Amazon, Google, Slack, Applause & others. Report results of debug logs & visual feedback.", "Sales & Marketing: Making bids, setting terms & prices, developing and implementing online marketing strategies"]
+      jobDescription: [ "Offer engineering services in projects such as: Robotic Control System Design & Software development (C/C++/Matlab); Mechanical design of movie props (Solidworks), Software Translation (C++/Python/Matlab)", "Offer consulting expertise in Multiple S/W & H/W functional testing projects for products, applications & websites from Amazon, Google, Slack, Applause & others. Report results of debug logs & visual feedback.", "Sales & Marketing: Making bids, setting terms & prices, developing and implementing online marketing strategies","Make 3D printed products on request & sell them. (COVID19: Adult Protective Face Gear, Adult Protect Chin Gear, Children Protective Face Gear)"]
     },
     {
       date:"Jan. 2019~ Aug.2019",
