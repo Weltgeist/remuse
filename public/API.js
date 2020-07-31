92ad6bbcb27940c4344329a7a78f8589e57e98ea
Weltgeist <narcisse118@gmail.com>
2020-07-31 00:04:40 -0400

    added a API.js with a GET and POST ex

--- public/API.js
+++ public/API.js
@@ -1,35 +1,70 @@
 // information to reach API
-const apiKey = '5653779151494d0d9fe8ba20cc64fdc7';
-const url = 'https://api.rebrandly.com/v1/links';
-
+const dataMuseUrl = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jjb=';
 
 // selecting page elements
 const inputField = document.querySelector('#input');
-const shortenButton = document.querySelector('#shorten');
+const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
 
 // AJAX function
-const shortenUrl = async () => {
-  const urlToShorten = inputField.value;
-  const data = JSON.stringify({destination: urlToShorten});
-  try {
-    const response = await fetch(url,{
-      method: 'POST',
-      body: data,
-      headers: {
-        'Content-type': 'application/json',
-        'apikey': apiKey
-      }
-    });
+const getSuggestions = async () => {
+  const wordQuery = inputField.value;
+  const endpoint = dataMuseUrl + queryParams + wordQuery;
+
+  try{
+    const response =  await fetch(endpoint);
     if(response.ok){
-      const jsonResponse = await response.json();
-      renderResponse(jsonResponse);
+      let jsonResponse = await response.json();
+			renderWordResponse(jsonResponse);
     }
-  } catch (error) {
+  }
+  catch(error){
     console.log(error);
   }
 }
 
+// clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
+
+// information to reach Rebrandly API
+const apiKey = '5653779151494d0d9fe8ba20cc64fdc7';
+const rebrandlyEndpoint = 'https://api.rebrandly.com/v1/links';
+
+// element selector
+const shortenButton = document.querySelector('#shorten');
+
+// AJAX functions
+const shortenUrl = async () =>{
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+
+  try{
+    const response =  await fetch(rebrandlyEndpoint, {
+      method: 'POST',
+      body: data,
+      headers: {
+        "Content-type": "application/json",
+        'apikey': apiKey
+      }
+    })
+    if(response.ok){
+      const jsonResponse = await response.json();
+			renderByteResponse(jsonResponse);
+    }
+  }
+  catch(error){
+    console.log(error);
+  }
+}
 
 // Clear page and call AJAX functions
 const displayShortUrl = (event) => {
